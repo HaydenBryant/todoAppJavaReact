66008a1ce720174f8cca4885559f38d917b258f6
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-06-14 23:14:19 -0400

    added authentication bean

---
+++ src/main/java/com/in28minutes/rest/webservice/restfulwebservices/AuthenticationBean.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservice.restfulwebservices;
+
+public class AuthenticationBean {
+    public AuthenticationBean() {
+    }
+
+    @Override
+    public String toString() {
+        return "AuthenticationBean{}";
+    }
+
+    public AuthenticationBean(String you_are_authenticated) {
+
+    }
+}
